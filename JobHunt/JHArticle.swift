95d10ac77fcd04900baad4a8af40cfd027d5c572
Kei Sato <bakurock15@gmail.com>
2014-12-20 18:51:54 +0900

    apiから情報とってくる

---
+++ JobHunt/JHArticle.swift
@@ -0,0 +1,13 @@
+//
+//  JHArticle.swift
+//  JobHunt
+//
+//  Created by kei on 2014/12/20.
+//  Copyright (c) 2014年 tm444. All rights reserved.
+//
+
+import UIKit
+
+class JHArticle: NSObject {
+   
+}
---
+++ JobHunt/JHCustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  JHCustomCell.swift
+//  JobHunt
+//
+//  Created by kei on 2014/12/20.
+//  Copyright (c) 2014年 tm444. All rights reserved.
+//
+
+import UIKit
+
+class JHCustomCell: UICollectionViewCell {
+    
+}
