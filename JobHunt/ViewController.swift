00a34257e840b9e9c05e7f7f3b3601d60c6b74c9
takumi <you@example.com>
2014-12-20 12:27:13 +0900

    プロジェクトを作成しました

---
+++ JobHunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JobHunt
+//
+//  Created by takumi on 2014/12/20.
+//  Copyright (c) 2014年 tm444. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
